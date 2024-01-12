d037ad5acf1227fa14340302b2920ce52f4d22fb
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-01-12 15:04:42 -0500

    updating routes for users

--- controllers/user-routes.js
+++ controllers/user-routes.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const {User} = require('../models/User');
 
+//#When the Create/Login button is selected from the homepage
+router.get('/CreateUserOrLogin', async (req,res) => {
+    res.render('login')
+})
+
+//#When settings button is selected from the homepage 
+router.get('/account', async (req,res) => {
+    res.render('account')
+})
+
 
 module.exports = router;
 
